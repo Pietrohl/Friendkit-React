d16cebb3a0213c539e8fea509cce1c93b777b59f
taufeeq849 <taufeeq849@gmail.com>
2020-11-28 16:28:35 +0200

    final working commit

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,84 +1,98 @@
-import React, { Component } from 'react';
-import friendkitWhite from '../assets/img/logo/friendkit-white.svg';
-import FadeIn from 'react-fade-in';
-import { Check, Lock, User } from 'react-feather';
-
+import React, { Component } from "react";
+import friendkitWhite from "../assets/img/logo/friendkit-white.svg";
+import FadeIn from "react-fade-in";
+import { Check, Lock, User } from "react-feather";
+import { Link } from "react-router-dom";
 
 export default class Login extends Component {
-
-    render() {
-        return (
-            <FadeIn>
-
-                <div className="login-wrapper columns is-gapless">
-                    {/*Left Side (desktop Only)*/}
-                    <div className="column is-6 is-hidden-mobile hero-banner">
-                        <div className="hero is-fullheight is-login">
-                            <div className="hero-body">
-                                <div className="container">
-                                    <div className="left-caption">
-                                        <h2>Join an Exciting Social Experience.</h2>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/*Right Side*/}
-                    <div className="column is-6">
-                        <div className="hero form-hero is-fullheight">
-                            {/*Logo*/}
-                            <div className="logo-wrap">
-                                <div className="wrap-inner">
-                                    <img src={friendkitWhite} alt="" />
-                                </div>
-                            </div>
-                            {/*Login Form*/}
-                            <div className="hero-body">
-                                <div className="form-wrapper">
-                                    {/*Avatar*/}
-                                    <div className="avatar">
-                                        <div className="badge">
-                                            <Check/>
-                                        </div>
-                                        <img src="https://placehold.it/128x128" data-demo-src="assets/img/avatars/jenna.png" alt="" />
-                                    </div>
-                                    {/*Form*/}
-                                    <div className="login-form">
-                                        <div className="field">
-                                            <div className="control">
-                                                <input className="input email-input" type="text" placeholder="jennadavis@gmail.com" />
-                                                <div className="input-icon">
-                                                    <User/>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="field">
-                                            <div className="control">
-                                                <input className="input password-input" type="password" placeholder="●●●●●●●" />
-                                                <div className="input-icon">
-                                                    <Lock />
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="field">
-                                            <div className="control">
-                                                <button className="button is-solid primary-button raised is-rounded is-fullwidth">Login</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="section forgot-password">
-                                        <div className="has-text-centered">
-                                            <a href="/login">Forgot password?</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  render() {
+    return (
+      <FadeIn>
+        <div className="login-wrapper columns is-gapless">
+          {/*Left Side (desktop Only)*/}
+          <div className="column is-6 is-hidden-mobile hero-banner">
+            <div className="hero is-fullheight is-login">
+              <div className="hero-body">
+                <div className="container">
+                  <div className="left-caption">
+                    <h2>Join an Exciting Social Experience.</h2>
+                  </div>
                 </div>
-            </FadeIn>
+              </div>
+            </div>
+          </div>
 
-        )
-    }
+          {/*Right Side*/}
+          <div className="column is-6">
+            <div className="hero form-hero is-fullheight">
+              {/*Logo*/}
+              <div className="logo-wrap">
+                <div className="wrap-inner">
+                  <img src={friendkitWhite} alt="" />
+                </div>
+              </div>
+              {/*Login Form*/}
+              <div className="hero-body">
+                <div className="form-wrapper">
+                  {/*Avatar*/}
+                  <div className="avatar">
+                    <div className="badge">
+                      <Check />
+                    </div>
+                    <img
+                      src="https://placehold.it/128x128"
+                      data-demo-src="assets/img/avatars/jenna.png"
+                      alt=""
+                    />
+                  </div>
+                  {/*Form*/}
+                  <div className="login-form">
+                    <div className="field">
+                      <div className="control">
+                        <input
+                          className="input email-input"
+                          type="text"
+                          placeholder="jennadavis@gmail.com"
+                        />
+                        <div className="input-icon">
+                          <User />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="field">
+                      <div className="control">
+                        <input
+                          className="input password-input"
+                          type="password"
+                          placeholder="●●●●●●●"
+                        />
+                        <div className="input-icon">
+                          <Lock />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="field">
+                      <div className="control">
+                        <Link
+                          to="/feed"
+                          className="button is-solid primary-button raised is-rounded is-fullwidth"
+                        >
+                          Login
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="section forgot-password">
+                    <div className="has-text-centered">
+                      <a href="/login">Forgot password?</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </FadeIn>
+    );
+  }
 }
